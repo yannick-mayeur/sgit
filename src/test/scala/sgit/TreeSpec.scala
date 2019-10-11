4c5971468170f7ac8229708cd5724a577d465db9
Yannick Mayeur <yannick.mayeur@gmail.com>
2019-10-11 10:13:49 +0200

    Update Tree tests

--- src/test/scala/sgit/TreeSpec.scala
+++ src/test/scala/sgit/TreeSpec.scala
@@ -12,10 +12,43 @@ class TreeSpec extends FlatSpec with Matchers {
   }
 
   "Tree" should "calculate hash based toString" in {
+    val tree =
+      Tree("azer", Seq(Tree("test", Seq(), Seq(Blob("foo", "bar")))), Seq())
+    val res =
+      FileStatus.getHashFor(
+        "Tree(azer,List(Tree(test,List(),List(Blob(foo,bar)))),List())"
+      )
+    tree.hash shouldBe res
+  }
+
+  it should "merge into another tree" in {
     val tree1 =
       Tree("azer", Seq(Tree("test", Seq(), Seq(Blob("foo", "bar")))), Seq())
     val tree2 =
-      Tree("azer", Seq(Tree("test", Seq(), Seq(Blob("foo", "bar")))), Seq())
-    assert(tree1.hash == tree2.hash)
+      Tree(
+        "azer",
+        Seq(
+          Tree(
+            "test",
+            Seq(Tree("mitre", Seq(), Seq())),
+            Seq(Blob("baz", "foobar"))
+          )
+        ),
+        Seq()
+      )
+    val merged = tree1.merge(tree2)
+    val res =
+      Tree(
+        "azer",
+        Seq(
+          Tree(
+            "test",
+            Seq(Tree("mitre", Seq(), Seq())),
+            Seq(Blob("foo", "bar"), Blob("baz", "foobar"))
+          )
+        ),
+        Seq()
+      )
+    merged shouldBe res
   }
 }
