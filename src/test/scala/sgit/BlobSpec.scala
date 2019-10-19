336992f856de20b370139e474ccdc4c7e4878f2b
Yannick Mayeur <yannick.mayeur@gmail.com>
2019-10-19 19:16:54 +0200

    Add tests for Blob

--- src/test/scala/sgit/BlobSpec.scala
+++ src/test/scala/sgit/BlobSpec.scala
@@ -1,6 +1,7 @@
 package sgit
 
 import org.scalatest._
+import scala.collection.mutable
 
 class BlobSpec extends FlatSpec with Matchers {
 
@@ -8,5 +9,45 @@ class BlobSpec extends FlatSpec with Matchers {
     val blob1 = Blob("foo", "foobarbaz")
     val blob2 = Blob("bar", "foobarbaz")
     assert(blob1.hash == blob2.hash)
+  }
+
+  it should "write to working directory" in {
+    var workingDirectory = mutable.Map.empty[String, String]
+    def writeToWorkingDirectory =
+      (name: Option[String]) =>
+        (content: String) => name.foreach(workingDirectory(_) = content)
+    val blob = Blob("/foo", "bar")
+    blob.toWorkingDirectory(writeToWorkingDirectory)
+    assert(workingDirectory.contains(blob.name))
+    workingDirectory(blob.name) shouldEqual "bar"
+  }
+
+  it should "write to repository" in {
+    var repository = mutable.Map.empty[String, String]
+    def writeToRepository =
+      (name: Option[String]) =>
+        (content: String) => name.foreach(repository(_) = content)
+    val blob = Blob("/foo", "bar")
+    blob.save(writeToRepository)
+    assert(repository.contains(blob.hash))
+    repository(blob.hash) shouldEqual "bar"
+  }
+
+  it should "load from working directory" in {
+    var workingDirectory = mutable.Map.empty[String, String]
+    val blob = Blob("/foo", "bar")
+    workingDirectory(blob.name.drop(1)) = blob.content
+    val getFromWorkingDirectory = (name: String) => Some(workingDirectory(name))
+    val loadedBlob = Blob.loadFromWD("/foo", getFromWorkingDirectory)
+    loadedBlob shouldEqual Some(blob)
+  }
+
+  it should "load from repository" in {
+    var repository = mutable.Map.empty[String, String]
+    val blob = Blob("/foo", "bar")
+    repository(blob.hash) = blob.content
+    val getFromRepository = (hash: String) => Some(repository(hash))
+    val loadedBlob = Blob.loadFromRepo(blob.name, blob.hash, getFromRepository)
+    loadedBlob shouldEqual Some(blob)
   }
 }
